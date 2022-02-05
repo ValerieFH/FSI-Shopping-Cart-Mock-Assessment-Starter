2145cb3e454414f26b30e21d483f7c1f424552b9
Valerie Haupt <valre@valeries-mbp-2.attlocal.net>
2022-02-05 12:32:18 -0600

    corrected js to use querySelector, remove unneeded HTML additions

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,26 @@
 let quantity = 1
-let quantityDisplay = document.getElementById('qty')
-let totalPrice = document.getElementById('ttl')
+let quantityDisplay = document.querySelector('.total-quantity')
+let totalPrice = document.querySelector('.total-price')
 
 let addOne = document.getElementById("quantity-up")
 addOne.addEventListener('click', function(){
     quantity++
-    quantityDisplay.textContent = quantity
-    totalPrice.textContent = `$${quantity*15}.00`
-    //console.log(quantity)
+    quantityDisplay.textContent = `Quantity: ${quantity}`
+    totalPrice.textContent = `Total Price: $${quantity*15}.00`
 })
 
 let subtractOne = document.getElementById("quantity-down")
 subtractOne.addEventListener('click', function(){
     if (quantity > 0){
         quantity--
-        quantityDisplay.textContent = quantity
-        totalPrice.textContent = `$${quantity*15}.00`
+        quantityDisplay.textContent = `Quantity: ${quantity}`
+        totalPrice.textContent = `Total Price: $${quantity*15}.00`
     }
-    //console.log(quantity)
 })
 
-let remove = document.getElementById('rmve')
+let remove = document.querySelector('.remove')
 remove.addEventListener('click',function(){
     quantity = 0
-    quantityDisplay.textContent = 0
-    totalPrice.textContent = `$${quantity*15}.00`
+    quantityDisplay.textContent = `Quantity: ${quantity}`
+    totalPrice.textContent = `Total Price: $${quantity*15}.00`
 })
